5d44525fc1524fe04ef67ba97598404d179ba2ea
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-30 00:54:22 +0100

    add some text in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <p className="text-2xl text-center"> test</p>;
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1 className="text-7xl font-bold p-5">Junior Developer</h1>
+      <h2 className="text-5xl font-bold p-5">Alexandre Pirmez</h2>
+    </main>
+  );
 }
