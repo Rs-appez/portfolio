9ac46dd5e3ef3dce1a55acd600b30f7903a665c7
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-30 00:53:20 +0100

    add placeholder for test

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,9 @@
+export type Project = {
+  name: string;
+  description: string;
+  placeholderImage: string;
+  images: string[];
+  link: string;
+  gh: string;
+  tags: string[];
+};
---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,11 @@
+const projects = [
+  {
+    name: "Project 1",
+    description: "This is a description of Project 1.",
+    placeholderImage: "",
+    images: [],
+    link: "https://example.com/project1",
+    gh: "",
+    tags: ["Tag1", "Tag2"],
+  },
+];
