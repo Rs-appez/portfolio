2c25aea9b7a955e7315d957f5da2ff7a09639051
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-30 18:05:06 +0200

    add endpoint

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1 className="text-7xl font-bold p-5">About</h1>
+      <p className="text-lg font-semibold p-5">
+        I am a junior developer with a passion for learning and growing in the
+        field of software development. I have experience in various programming
+        languages and frameworks, and I am always eager to take on new
+        challenges and expand my skill set.
+      </p>
+    </main>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1 className="text-7xl font-bold p-5">Projects</h1>
+    </main>
+  );
+}
