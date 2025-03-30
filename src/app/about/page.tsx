da7ec60d3be503ed88656cd79e98cb756ae6a3d1
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-30 19:12:55 +0200

    make about accurate

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <main className="flex flex-col items-center p-24">
+    <main className="flex flex-col items-center p-20 pb-0 ">
       <h1 className="text-7xl font-bold p-5">About</h1>
       <div className="flex items-center p-5">
         <Image
@@ -12,12 +12,45 @@ export default function About() {
           height={500}
           className="rounded-full border-4 border-foreground"
         />
-        <p className="text-lg font-semibold p-5">
-          I am a junior developer with a passion for learning and growing in the
-          field of software development. I have experience in various
-          programming languages and frameworks, and I am always eager to take on
-          new challenges and expand my skill set.
-        </p>
+        <div className="flex flex-col  item-left p-5">
+          <p className="text-lg font-semibold p-2 pb-5">
+            My name is Alexandre Pirmez, and I'm passionate about web
+            development, CI/CD and web security.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I mainly develop in Python (Django/Django REST Framework). In my
+            spare time, I like to develop various projects. I publish and deploy
+            them on my github.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I also have front-end development skills, mainly on Angular
+            projects.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I have experience with PostgreSQL and Redis databases.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I regularly use Git, Docker, and command line tools.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I'm familiar with AGILE development and SCRUM in particular.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I enjoy exploring new types of projects and continually expanding my
+            knowledge.
+          </p>
+
+          <p className="text-lg font-semibold p-2">
+            I am quick to adapt and learn in a new environment. I'm a good
+            self-starter.
+          </p>
+        </div>
       </div>
     </main>
   );
