86acc32d68c32bf4edeabb9a80da9a1f28508602
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-30 18:36:08 +0200

    add image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <main className="flex flex-col items-center p-24">
       <h1 className="text-7xl font-bold p-5">About</h1>
-      <p className="text-lg font-semibold p-5">
-        I am a junior developer with a passion for learning and growing in the
-        field of software development. I have experience in various programming
-        languages and frameworks, and I am always eager to take on new
-        challenges and expand my skill set.
-      </p>
+      <div className="flex items-center p-5">
+        <Image
+          src="/profile.png"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+          className="rounded-full border-4 border-gray-500"
+        />
+        <p className="text-lg font-semibold p-5">
+          I am a junior developer with a passion for learning and growing in the
+          field of software development. I have experience in various
+          programming languages and frameworks, and I am always eager to take on
+          new challenges and expand my skill set.
+        </p>
+      </div>
     </main>
   );
 }
