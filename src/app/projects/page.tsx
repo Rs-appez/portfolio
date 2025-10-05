9ea9826927e54d8ed1efe5881421a6fdc92e745a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-05 05:25:45 +0200

    format

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
-  return (
-    <main className="flex flex-col items-center p-20 pb-0 ">
-      <h1 className="text-7xl font-bold p-5">Projects</h1>
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center p-20 pb-0 ">
+            <h1 className="text-7xl font-bold p-5">Projects</h1>
+        </main>
+    );
 }
