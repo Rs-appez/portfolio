d52cf4de61e0eb5fd415774b05e5a6ba1c80af6e
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-05 04:25:35 +0200

    style(projects): adjust main padding for better layout
    Reduced main section padding from 24 to 20 and removed bottom padding
    to improve visual alignment on the Projects page.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <main className="flex flex-col items-center p-24">
+    <main className="flex flex-col items-center p-20 pb-0 ">
       <h1 className="text-7xl font-bold p-5">Projects</h1>
     </main>
   );
