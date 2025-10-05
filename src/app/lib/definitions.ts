8187cb69d7d42cd63f19364e0a89207405b81278
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-05 11:15:57 +0200

    change variable name

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,10 +1,11 @@
 export type Project = {
-  id: number;
-  name: string;
-  description: string;
-  placeholderImage: string;
-  images: string[];
-  link: string;
-  gh: string;
-  tags: string[];
+    id: number;
+    name: string;
+    description: string;
+    placeholderImage: string;
+    images: string[];
+    liveLink: string;
+    githubLink: string;
+    tags: string[];
+    linkedProjects: number[];
 };
