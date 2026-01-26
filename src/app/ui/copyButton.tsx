66a24863d91dd2a45bd912875044535d48b6ebff
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-26 02:39:29 +0100

    feat(ui): adjust CopyButton tooltip position and style
    Refactored the CopyButton component to improve tooltip visibility and
    alignment. The tooltip is now positioned higher above the button using
    the '-top-8' class, ensuring it does not overlap with the button itself.
    Also improved code indentation for better readability.

--- src/app/ui/copyButton.tsx
+++ src/app/ui/copyButton.tsx
@@ -5,32 +5,32 @@ import clsx from "clsx";
 import Image from "next/image";
 
 export default function CopyButton({ text }: { text: string }) {
-  const [tooltipText, setTooltipText] = useState("Copy to clipboard");
+    const [tooltipText, setTooltipText] = useState("Copy to clipboard");
 
-  const handleCopy = () => {
-    navigator.clipboard.writeText(text);
-    setTooltipText("Copied!");
-    setTimeout(() => setTooltipText("Copy to clipboard"), 2000);
-  };
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        setTooltipText("Copied!");
+        setTimeout(() => setTooltipText("Copy to clipboard"), 2000);
+    };
 
-  return (
-    <div className="relative group inline-flex ">
-      <span
-        className={clsx(
-          "absolute left-1/2 transform -translate-x-1/2 top-0 mb-1 hidden w-max rounded px-2 py-1 text-xs text-white group-hover:block",
-          tooltipText === "Copied!" ? "bg-green-700" : "bg-gray-700",
-        )}
-      >
-        {tooltipText}
-      </span>
-      <Image
-        src="/copyButton.svg"
-        alt="Copy"
-        width={20}
-        height={20}
-        className="ml-2 cursor-pointer dark:invert"
-        onClick={handleCopy}
-      />
-    </div>
-  );
+    return (
+        <div className="relative group inline-flex ">
+            <span
+                className={clsx(
+                    "absolute left-1/2 transform -translate-x-1/2 -top-8 mb-1 hidden w-max rounded px-2 py-1 text-xs text-white group-hover:block",
+                    tooltipText === "Copied!" ? "bg-green-700" : "bg-gray-700",
+                )}
+            >
+                {tooltipText}
+            </span>
+            <Image
+                src="/copyButton.svg"
+                alt="Copy"
+                width={20}
+                height={20}
+                className="ml-2 cursor-pointer dark:invert"
+                onClick={handleCopy}
+            />
+        </div>
+    );
 }
